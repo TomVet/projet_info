dd7246e7a1cd5726c3a5212271670d54598c4905
Tom Dion <Tom.Dion@etu.sorbonne-universite.fr>
2021-11-22 16:06:02 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 19 12:03:56 2021
+
+@author: fireb
+"""
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 test = np.array([(1,3),(3,2),(9,4)])
 
-def centroide(liste_coordonne):
+def coordonnees_centroide(liste_coordonne):
     """
     Calcule le centroide des points de liste_coordonne dans de dimension
     nb_dimension
@@ -14,11 +22,11 @@ def centroide(liste_coordonne):
 
     Returns
     -------
-    coordonnee_centroide : np.array
+    coordonnees : np.array
         liste des coordonnee du centroide calcule.
 
     """
-    coordonnee_centroide = np.array([])
+    coordonnees = np.array([])
     # on calcule la dimension de l'espace considérer pour le centroide
     nb_dimension = len(liste_coordonne[1])
     # on calcul les coordonnees du centroide dans chaque dimension
@@ -27,12 +35,12 @@ def centroide(liste_coordonne):
         # on somme les coordonnees de chaque point
         for point in liste_coordonne:
             somme += point[dimension]
-        # on ajoute la somme / par le nombre de point a coordonne_centroide
-        coordonnee_centroide = np.append(coordonnee_centroide, [somme/len(liste_coordonne)])
-    return coordonnee_centroide
+        # on ajoute la somme / par le nombre de point a coordonnees
+        coordonnees = np.append(coordonnees, [somme/len(liste_coordonne)])
+    return coordonnees
 
 def distance_euclidienne(point_1, point_2):
-   """
+    """
     Calcul de la distance euclidienne au carre entre les points 1 et 2
 
     Parameters
@@ -48,20 +56,37 @@ def distance_euclidienne(point_1, point_2):
         distance euclidienne au carre entre les point 1 et 2.
 
     """
-   # on calcule la dimension de l'espace des 2 points
-   nb_dimension = len(point_1)
-   distance = 0
-   for dimension in range(nb_dimension):
-       somme = (point_1[dimension] - point_2[dimension])**2
-       distance += somme
-   return distance
+    # on calcule la dimension de l'espace des 2 points
+    nb_dimension = len(point_1)
+    distance = 0
+    for dimension in range(nb_dimension):
+        somme = (point_1[dimension] - point_2[dimension])**2
+        distance += somme
+    return distance
 
-centroides = {''}
 
 def centroide_proche(point, centroides):
+    """
+    permet de trouver le centroide le plus proche du point
+
+    Parameters
+    ----------
+    point : np.array
+        liste des coordonnee du point.
+    centroides : np.array
+        liste de coordonnee de centroides.
+
+    Returns
+    -------
+    indice_du_min : int
+        indice du centroide le plus proche de point dans la liste centroide.
+
+    """
     distance_min = 0
-    rang_du_min = 0
-    for centroide in centroides:
+    indice_du_min = 0
+    for indice, centroide in enumerate(centroides):
         distance = distance_euclidienne(point, centroide)
         if distance_min < distance:
             distance_min = distance
+            indice_du_min = indice
+    return indice_du_min
