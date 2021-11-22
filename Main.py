f81c9f03b5b8fa8c0305a49a743b4f83b05c3a57
Tom Dion <Tom.Dion@etu.sorbonne-universite.fr>
2021-11-22 15:48:01 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -22,18 +22,18 @@ def centroide(liste_coordonne):
     # on calcule la dimension de l'espace considérer pour le centroide
     nb_dimension = len(liste_coordonne[1])
     # on calcul les coordonnees du centroide dans chaque dimension
-    for k in range(nb_dimension):
+    for dimension in range(nb_dimension):
         somme = 0
         # on somme les coordonnees de chaque point
-        for i in liste_coordonne:
-            somme += i[k]
+        for point in liste_coordonne:
+            somme += point[dimension]
         # on ajoute la somme / par le nombre de point a coordonne_centroide
         coordonnee_centroide = np.append(coordonnee_centroide, [somme/len(liste_coordonne)])
     return coordonnee_centroide
 
 def distance_euclidienne(point_1, point_2):
    """
-    Calcul de la distance euclidienne entre les points 1 et 2
+    Calcul de la distance euclidienne au carre entre les points 1 et 2
 
     Parameters
     ----------
@@ -45,22 +45,23 @@ def distance_euclidienne(point_1, point_2):
     Returns
     -------
     distance : float
-        distance euclidienne entre les point 1 et 2.
+        distance euclidienne au carre entre les point 1 et 2.
 
     """
-   # on calcule la dimension de l'espace des 2 ponits
+   # on calcule la dimension de l'espace des 2 points
    nb_dimension = len(point_1)
    distance = 0
-   for k in range(nb_dimension):
-       somme = (point_1[k] - point_2[k])**2
+   for dimension in range(nb_dimension):
+       somme = (point_1[dimension] - point_2[dimension])**2
        distance += somme
-   distance = np.sqrt(distance)
    return distance
 
 centroides = {''}
 
 def centroide_proche(point, centroides):
-
-
-
-print(centroide(test))
+    distance_min = 0
+    rang_du_min = 0
+    for centroide in centroides:
+        distance = distance_euclidienne(point, centroide)
+        if distance_min < distance:
+            distance_min = distance
