ecec352b8f06afddd260d5d15381e1d29d85a069
TomVet <90911421+TomVet@users.noreply.github.com>
2021-11-25 11:01:11 +0100

    Update nearest_centroide.py
    Ajout de commentaire

--- nearest_centroide.py
+++ nearest_centroide.py
@@ -59,6 +59,7 @@ def distance_euclidienne(point_1, point_2):
     # on calcule la dimension de l'espace des 2 points
     nb_dimension = len(point_1)
     distance = 0
+    # on fait la somme au carre des coordonnees des points 1 et 2 dans chaque dimension
     for dimension in range(nb_dimension):
         somme = (point_1[dimension] - point_2[dimension])**2
         distance += somme
@@ -84,37 +85,54 @@ def centroide_proche(point, centroides):
     """
     distance_min = 0
     indice_du_min = 0
+    # on parcour la liste des centroides
     for indice, centroide in enumerate(centroides):
+        # on calcul la distance entre le centroide et le point
         distance = distance_euclidienne(point, centroide)
-        if distance_min < distance:
+        # si la nouvelle distance est plus petite que le minimum elle devient le minimum
+        if distance_min > distance:
             distance_min = distance
+            # on conserve l´indice, dans la liste de centroides, du centroide le plus proche
             indice_du_min = indice
     return indice_du_min
 
+"""
+on cree un dataset en 2 dimension pour tester l´algorythme et pouvoir le visualiser
+nombre d'echantillon : 50
+nombre de parametre pour definir un point : 2
+nombre de parametre informatif : 2
+nombre de parametre redondant : 0
+nombre de classe = 3
+nombre de groupe par classe = 1
+"""
 data = dataset.make_classification(n_samples=50, n_features=2, n_informative=2, n_redundant=0, n_classes=3, n_clusters_per_class=1)
 
+
+'''on separe le dataset en fonction des classes'''
+# on cherche ou sont les points de la classe 1 dans la liste data
 coor_classe_1 = np.where(data[1] == 0)
+# on met les coordonnees des points de la classe 1 dans une nouvelle liste
 classe_1 = [data[0][point] for point in coor_classe_1]
+# on calcule le centroide de la classe
 centroide_1 = coordonnees_centroide(classe_1)
-
-coor_classe_2 = np.where(data[1] == 1)
-classe_2 =  np.array([data[0][point] for point in coor_classe_2])
-
-coor_classe_3 = np.where(data[1] == 2)
-classe_3 =  np.array([data[0][point] for point in coor_classe_3])
-
-centroide_1 = coordonnees_centroide(classe_1)
-centroide_2 = coordonnees_centroide(classe_2)
-centroide_3 = coordonnees_centroide(classe_3)
-
 x_1 = centroide_1[0]
 y_1 = centroide_1[1]
 
+# on cherche ou sont les points de la classe 2 dans la liste data
+coor_classe_2 = np.where(data[1] == 1)
+# on met les coordonnees des points de la classe 2 dans une nouvelle liste
+classe_2 =  np.array([data[0][point] for point in coor_classe_2])
+# on calcule le centroide
+centroide_2 = coordonnees_centroide(classe_2)
 x_2 = centroide_2[0]
 y_2 = centroide_2[1]
 
+coor_classe_3 = np.where(data[1] == 2)
+classe_3 =  np.array([data[0][point] for point in coor_classe_3])
+centroide_3 = coordonnees_centroide(classe_3)
 x_3 = centroide_3[0]
 y_3 = centroide_3[1]
+
 
 plt.plot(x_1, y_1, 'bo')
 plt.plot(x_2, y_2, 'ro')
