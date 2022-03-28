1625e2ee4081461d6675578ca7fa2f630757e1e4
jeanbafdsg575s5dg5 <92884198+jeanbafdsg575s5dg5@users.noreply.github.com>
2022-03-28 17:28:58 +0200

    Update naives_bayes_4.py

--- naives_bayes_4.py
+++ naives_bayes_4.py
@@ -1,9 +1,8 @@
 from math import *
-from sympy import *
 
 def calcul_ecart(points):
     classes = set(point[-1] for point in points)
-    return min(classes)
+    return int(min(classes))
 
 def calcul_esp(points):
 # renvoie lesperance de chaque catégorie en fonction de sa classe
@@ -40,6 +39,7 @@ def calcul_var(points,ecart):
             for point in points:
                 if point[-1] == classe:
                     compteur += 1
+                    #print(compteur)
                     somme = somme + (point[rang] - calcul_esp(points)[int(classe-ecart)][0][int(rang)])**2
                     #print(somme)
             variance.append((somme/(compteur-1)))
@@ -50,9 +50,9 @@ def calcul_var(points,ecart):
     return variance_par_classe
 
 C=recuperer_donnee_csv("C:/Users/jbcor/Downloads/dataset.csv")
-B=[[0.997,3.65]]
+D=[[0.997,3.65]]
 A=[[182,81.6,30,1],[180,86.2,28,1],[170,77.1,30,1],[180,74.8,25,1],[152,45.4,15,0],[168,68,20,0],[165,59,18,0],[175,68,23,0]]
-
+B=[[183,59,20]]
 def calcul_proba_classe(points):
 # ex male ou femelle
     liste_classe = set([point[-1] for point in points])
@@ -65,11 +65,28 @@ def calcul_proba_classe(points):
         liste_proba.append([compteur/len(points),classe])
     return liste_proba
 
+
 def calcul_proba_categorie_sachant_classe(point,points,categorie,classe,ecart):
     esp = calcul_esp(points)[int(classe-ecart)][0][categorie]
     var = calcul_var(points,ecart)[int(classe-ecart)][0][categorie]
     proba = exp((-((point[0][categorie]-esp)**2)/(2*var)))/(sqrt(2*float(pi)*var))
     return proba
+
+
+def calcul_constante(points,ecart,point):
+    classes = set(point[-1] for point in points)
+    proba=1
+    constante=0
+    liste_proba=[]
+    for classe in classes:
+        for i in range(len(points[0])-1):
+            proba=proba*calcul_proba_categorie_sachant_classe(point,points,i,classe,ecart)
+        proba=proba*float(calcul_proba_classe(points)[int(classe-ecart)][0])
+        liste_proba.append(proba)
+        proba=1
+    for i in range(len(liste_proba)):
+        constante+=liste_proba[i]
+    return constante
 
 def calcul_proba_bayes(point,points):
     ecart=calcul_ecart(points)
@@ -81,5 +98,5 @@ def calcul_proba_bayes(point,points):
         proba_categorie_sachant_classe = 1
         for i in range(len(points[0])-1):
             proba_categorie_sachant_classe = proba_categorie_sachant_classe*calcul_proba_categorie_sachant_classe(point,points,i,classe,ecart)
-        liste_proba.append([(proba_classe*proba_categorie_sachant_classe),classe])
+        liste_proba.append([(proba_classe*proba_categorie_sachant_classe/calcul_constante(points,ecart,point)),classe])
     return liste_proba,max(liste_proba)[1]
